406fc23d9c4a5b6b671783fa79a54f209a971d39
Jeremiah Herring <jeremherring@gmail.com>
2024-03-08 13:13:36 -0800

    street lights

---
+++ website/views.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, render_template, request
+from LexerFSM.main import lexer
+
+
+views = Blueprint('views', __name__)
+
+@views.route('/')
+def home():
+    return render_template("home.html")
+
+@views.route("/lexical-analyzer", methods=['GET', 'POST'])
+def lexical_analyer():
+    if request.method == 'POST':
+        # Get the code from the textarea
+        code = request.form['textareaInput']
+
+        results = lexer(code)
+        return render_template("lexical-analyzer-results.html", results=results)
+    return render_template("lexical-analyzer.html")
+
+@views.route("/syntax-analyzer")
+def syntax_analyer():
+    return render_template("syntax-analyzer.html")
